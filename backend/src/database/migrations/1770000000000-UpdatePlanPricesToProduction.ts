5a8563fe70f89e385fb4d52113dbe9369b1cddc1
LONTSI TIOTSOP CEDRIC <lontsi05@gmail.com>
2026-02-06 20:36:51 +0100

    Add migration to update plan prices to production values

---
+++ backend/src/database/migrations/1770000000000-UpdatePlanPricesToProduction.ts
@@ -0,0 +1,69 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class UpdatePlanPricesToProduction1770000000000
+  implements MigrationInterface
+{
+  name = 'UpdatePlanPricesToProduction1770000000000';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // Update Standard plan prices
+    await queryRunner.query(`
+      UPDATE "plan"
+      SET "priceMonthlyXAF" = 19000,
+          "priceAnnualXAF" = 190000,
+          "priceMonthlyUSD" = 29,
+          "priceAnnualUSD" = 290
+      WHERE "code" = 'standard'
+    `);
+
+    // Update Pro plan prices
+    await queryRunner.query(`
+      UPDATE "plan"
+      SET "priceMonthlyXAF" = 32000,
+          "priceAnnualXAF" = 320000,
+          "priceMonthlyUSD" = 49,
+          "priceAnnualUSD" = 490
+      WHERE "code" = 'pro'
+    `);
+
+    // Update Enterprise plan prices
+    await queryRunner.query(`
+      UPDATE "plan"
+      SET "priceMonthlyXAF" = 130000,
+          "priceAnnualXAF" = 1300000,
+          "priceMonthlyUSD" = 199,
+          "priceAnnualUSD" = 1990
+      WHERE "code" = 'enterprise'
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // Revert to previous test prices (if needed)
+    await queryRunner.query(`
+      UPDATE "plan"
+      SET "priceMonthlyXAF" = 2000,
+          "priceAnnualXAF" = 20000,
+          "priceMonthlyUSD" = 2,
+          "priceAnnualUSD" = 20
+      WHERE "code" = 'standard'
+    `);
+
+    await queryRunner.query(`
+      UPDATE "plan"
+      SET "priceMonthlyXAF" = 3000,
+          "priceAnnualXAF" = 30000,
+          "priceMonthlyUSD" = 3,
+          "priceAnnualUSD" = 30
+      WHERE "code" = 'pro'
+    `);
+
+    await queryRunner.query(`
+      UPDATE "plan"
+      SET "priceMonthlyXAF" = 5000,
+          "priceAnnualXAF" = 50000,
+          "priceMonthlyUSD" = 5,
+          "priceAnnualUSD" = 50
+      WHERE "code" = 'enterprise'
+    `);
+  }
+}
